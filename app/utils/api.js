2c18c2dd6fbb0e5299cb1f525061e9d267472bd2
Mike <aolie94@gmail.com>
2017-06-09 08:40:35 -0700

    initial commit

---
+++ app/utils/api.js
@@ -0,0 +1,12 @@
+var axios = require('axios');
+
+module.exports = {
+  fetchWeather: function(area) {
+    var encodedURI = window.encodeURI('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + area + '&appid=615e677e85986ec9db1b638178542c28');
+
+    return axios.get(encodedURI)
+      .then(function(response){
+        return response.data.items;
+      })
+  }
+}
